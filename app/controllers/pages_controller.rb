7e312696e219091e7cdeed7efc8a7e3ae1ddb057
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2016-07-23 21:21:18 +0400

    Added most of the profile view code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,10 @@ class PagesController < ApplicationController
   def profile
   	@posts = Post.all
     @newPost = Post.new
+    @toFollow = User.all.last(5)
+    @post_form = "<%= render '/components/post_form' %><br><br>
+ needs to be here"
+    @follow_button = "<%= render '/components/follow_button', :user => @user %> needs to be here"
     @followers_count = "<%= @user.followers.count %> needs to be here after I create follow relationships and @user member variable corresponding to the page I'm on"
   end
 
