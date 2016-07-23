1aa256fede2cd4f3a3af01b041e22e6acbc6fad8
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2016-07-23 20:26:50 +0400

    Added some view code, working on adding dynamic images now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,16 @@ class PagesController < ApplicationController
   before_action :set_auth
 
   def home
+  	@posts = Post.all
+    @newPost = Post.new
+    @post_form = "<%= render '/components/post_form' %><br> needs to be there"
+    @followers_count = "<%= current_user.followers.count %> needs to be here"
   end
 
   def profile
+  	@posts = Post.all
+    @newPost = Post.new
+    @followers_count = "<%= @user.followers.count %> needs to be here after I create follow relationships and @user member variable corresponding to the page I'm on"
   end
 
   private
