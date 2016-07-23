0f21ebbd1f0067edc02e64471353d499d3b0f561
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2016-07-23 20:04:02 +0400

    Added Posts model, migrated it, added the belongs_to/has_many relationship with the user, to the corresponding models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+    
+    has_many :posts
+    
+    # helper methods
 
 	def self.sign_in_from_omniauth(auth)
 		find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
