e84cc8aba90b5b7d81868caafdbfe988e6e6458f
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2016-07-27 20:20:39 +0400

    Added follow/unfollow relationship, fixed some minor UI bugs, added follow button component

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,21 +4,21 @@ class PagesController < ApplicationController
   def home
   	@posts = Post.all
     @newPost = Post.new
-    @followers_count = "<%= current_user.followers.count %> needs to be here"
+    @followers_count = current_user.followers.count
   end
 
   def profile
     if (User.find_by(:id => params[:id]))
       @id = params[:id]
-      @profile_image = User.find_by(:id => params[:id]).profile_image
+      @user = User.find_by(:id => params[:id])
+      @profile_image = @user.profile_image
     else
       redirect_to root_path, :notice=> "User not found"
     end
   	@posts = Post.all.where("user_id = ?", User.find_by(:id => @id))
     @newPost = Post.new
     @toFollow = User.all.last(5)
-    @follow_button = "<%= render '/components/follow_button', :user => @user %> needs to be here"
-    @followers_count = "<%= @user.followers.count %> needs to be here after I create follow relationships and @user member variable corresponding to the page I'm on"
+    @followers_count = @user.followers.count
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
     
-    has_many :posts
-    
-    # helper methods
+    has_many :posts, dependent: :destroy # remove a user's posts if his account is deleted.
+	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+         
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
 
+    # helper methods
 	def self.sign_in_from_omniauth(auth)
 		find_by(provider: auth['provider'], uid: auth['uid'], profile_image: auth['info']['image']) || create_user_from_omniauth(auth)
 	end
@@ -15,5 +19,17 @@ class User < ActiveRecord::Base
 			name: auth['info']['name'],
 			profile_image: auth['info']['image']
 			)
+	end
+    # follow another user
+	def follow(other)
+		active_relationships.create(followed_id: other.id)
+	end
+	# unfollow a user
+	def unfollow(other)
+		active_relationships.find_by(followed_id: other.id).destroy
+	end  
+	# is following a user?
+	def following?(other)
+		following.include?(other)
 	end
 end
